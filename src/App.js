8e4c264d0dbddd3f754511e0c056fc456373c301
Sj3777 <sudhakarjha819@gmail.com>
2022-06-05 17:31:36 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Navbar } from "./Navbar";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence>
+      <div className="w-screen h-auto flex flex-col bg-primary ">
+        <Navbar />
+        <main className="mt-24 p-8 w-full">Maintainer: Container</main>
+      </div>
+    </AnimatePresence>
   );
 }
 
